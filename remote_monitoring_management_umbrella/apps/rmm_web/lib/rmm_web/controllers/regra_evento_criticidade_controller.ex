2d332fd37916a101538e26e5191e5282c2925622
Wilson Marcon Da Silva <wilson.marcon.silva@outlook.com>
2024-07-05 00:08:49 -0300

    RMM-39

--- remote_monitoring_management_umbrella/apps/rmm_web/lib/rmm_web/controllers/regra_evento_criticidade_controller.ex
+++ remote_monitoring_management_umbrella/apps/rmm_web/lib/rmm_web/controllers/regra_evento_criticidade_controller.ex
@@ -2,7 +2,7 @@ defmodule RmmWeb.RegraEventoCriticidadeController do
   use RmmWeb, :controller
 
   alias Rmm.EstruturasDeDados.Entidades
-  alias Rmm.EstruturasDeDados.Entidades.RegraEventoCriticidade
+  alias Rmm.Repo
 
   def index(conn, _params) do
     regras_eventos_criticidade = Entidades.list_regras_eventos_criticidade()
@@ -10,31 +10,53 @@ defmodule RmmWeb.RegraEventoCriticidadeController do
   end
 
   def new(conn, _params) do
-    changeset = Entidades.change_regra_evento_criticidade(%RegraEventoCriticidade{})
-    render(conn, :new, changeset: changeset)
+    changeset = Entidades.change_regra_evento_criticidade(%Rmm.EstruturasDeDados.Entidades.RegraEventoCriticidade{})
+    itens_configuracao = Repo.all(Rmm.EstruturasDeDados.Entidades.ItemConfiguracao)
+    solucoes_contorno = Repo.all(Rmm.EstruturasDeDados.Entidades.SolucaoContorno)
+
+    render(conn, :new, changeset: changeset, itens_configuracao: itens_configuracao, solucoes_contorno: solucoes_contorno)
   end
 
   def create(conn, %{"regra_evento_criticidade" => regra_evento_criticidade_params}) do
     case Entidades.create_regra_evento_criticidade(regra_evento_criticidade_params) do
       {:ok, regra_evento_criticidade} ->
         conn
-        |> put_flash(:info, "Regra evento criticidade created successfully.")
+        |> put_flash(:info, "Regra de Evento de Criticidade criada com sucesso.")
         |> redirect(to: ~p"/regras_eventos_criticidade/#{regra_evento_criticidade}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :new, changeset: changeset)
+        itens_configuracao = Repo.all(Rmm.EstruturasDeDados.Entidades.ItemConfiguracao)
+        solucoes_contorno = Repo.all(Rmm.EstruturasDeDados.Entidades.SolucaoContorno)
+
+        render(conn, :new, changeset: changeset, itens_configuracao: itens_configuracao, solucoes_contorno: solucoes_contorno)
     end
   end
 
   def show(conn, %{"id" => id}) do
     regra_evento_criticidade = Entidades.get_regra_evento_criticidade!(id)
-    render(conn, :show, regra_evento_criticidade: regra_evento_criticidade)
+
+    solucao_contorno = if regra_evento_criticidade.solucao_contorno_id do
+      Repo.get_by(Rmm.EstruturasDeDados.Entidades.SolucaoContorno, id: regra_evento_criticidade.solucao_contorno_id)
+    else
+      nil
+    end
+
+    item_config = if regra_evento_criticidade.item_configuracao_id do
+      Repo.get_by(Rmm.EstruturasDeDados.Entidades.ItemConfiguracao, id: regra_evento_criticidade.item_configuracao_id)
+    else
+      nil
+    end
+
+    render(conn, :show, regra_evento_criticidade: regra_evento_criticidade, item_config: item_config, solucao_contorno: solucao_contorno)
   end
 
   def edit(conn, %{"id" => id}) do
     regra_evento_criticidade = Entidades.get_regra_evento_criticidade!(id)
     changeset = Entidades.change_regra_evento_criticidade(regra_evento_criticidade)
-    render(conn, :edit, regra_evento_criticidade: regra_evento_criticidade, changeset: changeset)
+    itens_configuracao = Repo.all(Rmm.EstruturasDeDados.Entidades.ItemConfiguracao)
+    solucoes_contorno = Repo.all(Rmm.EstruturasDeDados.Entidades.SolucaoContorno)
+
+    render(conn, :edit, regra_evento_criticidade: regra_evento_criticidade, changeset: changeset, itens_configuracao: itens_configuracao, solucoes_contorno: solucoes_contorno)
   end
 
   def update(conn, %{"id" => id, "regra_evento_criticidade" => regra_evento_criticidade_params}) do
@@ -43,7 +65,7 @@ defmodule RmmWeb.RegraEventoCriticidadeController do
     case Entidades.update_regra_evento_criticidade(regra_evento_criticidade, regra_evento_criticidade_params) do
       {:ok, regra_evento_criticidade} ->
         conn
-        |> put_flash(:info, "Regra evento criticidade updated successfully.")
+        |> put_flash(:info, "Regra de Evento de Criticidade atualizada com sucesso.")
         |> redirect(to: ~p"/regras_eventos_criticidade/#{regra_evento_criticidade}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
@@ -56,7 +78,7 @@ defmodule RmmWeb.RegraEventoCriticidadeController do
     {:ok, _regra_evento_criticidade} = Entidades.delete_regra_evento_criticidade(regra_evento_criticidade)
 
     conn
-    |> put_flash(:info, "Regra evento criticidade deleted successfully.")
+    |> put_flash(:info, "Regra de Evento de Criticidade deletada com sucesso.")
     |> redirect(to: ~p"/regras_eventos_criticidade")
   end
 end
